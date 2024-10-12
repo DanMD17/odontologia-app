f4073d20ad96363e5d318364c1f20a8b627b4754
Paola Rengifo <pololita1015@gmail.com>
2024-10-12 02:17:12 -0500

    Se agregan los metodos de la clase Materiales

--- WebAppOdontologia/Data/MaterialsDat.cs
+++ WebAppOdontologia/Data/MaterialsDat.cs
@@ -7,5 +7,113 @@ namespace Data
 {
     public class MaterialsDat
     {
+        Persistence objPer = new Persistence();
+
+        //Metodo para mostrar todos los Usuarios
+        public DataSet showMaterials()
+        {
+            MySqlDataAdapter objAdapter = new MySqlDataAdapter();
+            DataSet objData = new DataSet();
+
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.openConnection();
+            objSelectCmd.CommandText = "spSelectMaterial";
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+            objAdapter.SelectCommand = objSelectCmd;
+            objAdapter.Fill(objData);
+            objPer.closeConnection();
+            return objData;
+        }
+
+        // Método para guardar un nuevo Material
+        public bool saveMaterial(string nombre, string descripcion, int cantidad, int trataId)
+        {
+            bool executed = false;
+            int row;
+
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.openConnection();
+            objSelectCmd.CommandText = "spInsertMaterial";
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+            objSelectCmd.Parameters.Add("p_nombre", MySqlDbType.VarString).Value = nombre;
+            objSelectCmd.Parameters.Add("p_descripcion", MySqlDbType.Text).Value = descripcion;
+            objSelectCmd.Parameters.Add("p_cantidad", MySqlDbType.Int32).Value = cantidad;
+            objSelectCmd.Parameters.Add("p_trata_id", MySqlDbType.Int32).Value = trataId;
+
+            try
+            {
+                row = objSelectCmd.ExecuteNonQuery();
+                if (row == 1)
+                {
+                    executed = true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.ToString());
+            }
+            objPer.closeConnection();
+            return executed;
+        }
+
+        // Método para actualizar un Material
+        public bool updateMaterial(int materialId, string nombre, string descripcion, int cantidad, int trataId)
+        {
+            bool executed = false;
+            int row;
+
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.openConnection();
+            objSelectCmd.CommandText = "spUpdateMaterial";
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+            objSelectCmd.Parameters.Add("p_mate_id", MySqlDbType.Int32).Value = materialId;
+            objSelectCmd.Parameters.Add("p_nombre", MySqlDbType.VarString).Value = nombre;
+            objSelectCmd.Parameters.Add("p_descripcion", MySqlDbType.Text).Value = descripcion;
+            objSelectCmd.Parameters.Add("p_cantidad", MySqlDbType.Int32).Value = cantidad;
+            objSelectCmd.Parameters.Add("p_trata_id", MySqlDbType.Int32).Value = trataId;
+
+            try
+            {
+                row = objSelectCmd.ExecuteNonQuery();
+                if (row == 1)
+                {
+                    executed = true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.ToString());
+            }
+            objPer.closeConnection();
+            return executed;
+        }
+
+        // Método para eliminar un Material
+        public bool deleteMaterial(int materialId)
+        {
+            bool executed = false;
+            int row;
+
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.openConnection();
+            objSelectCmd.CommandText = "spDeleteMaterial";
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+            objSelectCmd.Parameters.Add("p_mate_id", MySqlDbType.Int32).Value = materialId;
+
+            try
+            {
+                row = objSelectCmd.ExecuteNonQuery();
+                if (row == 1)
+                {
+                    executed = true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.ToString());
+            }
+            objPer.closeConnection();
+            return executed;
+        }
     }
 }
