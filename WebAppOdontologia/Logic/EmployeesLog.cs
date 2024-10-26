d4545777d56cd7793a649f7dca6fc0405e11ceab
Daniel Meza <daniel733xd@gmail.com>
2024-10-25 21:56:33 -0500

    Se corrigen los comentarios y se agrega el metodo eliminar para la clase EmployeesLog

--- WebAppOdontologia/Logic/EmployeesLog.cs
+++ WebAppOdontologia/Logic/EmployeesLog.cs
@@ -12,28 +12,34 @@ namespace Logic
         
         EmployeesDat objEmp = new EmployeesDat();
 
-        //Metodo para mostrar unicamente el id y la descripcion de los Provedores, en el DropDownList
+        //Metodo para mostrar unicamente el id y la descripcion de los empleados en DDL
         public DataSet showEmployeesDDL()
         {
             return objEmp.showEmployeesDDL();
         }
 
-        //Metodo para mostrar los Provedores
+        //Metodo para mostrar los empleados
         public DataSet showEmployees()
         {
             return objEmp.showEmployees();
         }
 
-        //Metodo para guardar un nuevo Proveedor
+        //Metodo para guardar un nuevo empleado
         public bool saveEmployee(int _identificacion, string _nombre, string _apellidos, string _celular, string _direccion, string _correo)
         {
             return objEmp.saveEmployee(_identificacion, _nombre, _apellidos, _celular, _direccion, _correo);
         }
 
-        //Metodo para actualizar un Proveedor
+        //Metodo para actualizar un empleado
         public bool updateEmployee(int _id, int _identificacion, string _nombre, string _apellidos, string _celular, string _direccion, string _correo)
         {
             return objEmp.updateEmployee(_id, _identificacion, _nombre, _apellidos, _celular, _direccion, _correo);
+        }
+
+        //Metodo para borrar un empleado
+        public bool deleteEmployee(int _idEmployee)
+        {
+            return objCat.deleteEmployee(_idEmployee);
         }
     }
 }
