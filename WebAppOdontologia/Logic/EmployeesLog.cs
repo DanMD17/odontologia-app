5c0b170087e7d46f66ce9848e5685cf34364edcc
Daniel Meza <daniel733xd@gmail.com>
2024-10-25 20:38:03 -0500

    correccion del nombre del obj en la clase employeesLog

--- WebAppOdontologia/Logic/EmployeesLog.cs
+++ WebAppOdontologia/Logic/EmployeesLog.cs
@@ -10,30 +10,30 @@ namespace Logic
     public class EmployeesLog
     {
         
-        EmployeesDat objPrv = new EmployeesDat();
+        EmployeesDat objEmp = new EmployeesDat();
 
         //Metodo para mostrar unicamente el id y la descripcion de los Provedores, en el DropDownList
         public DataSet showEmployeesDDL()
         {
-            return objPrv.showEmployeesDDL();
+            return objEmp.showEmployeesDDL();
         }
 
         //Metodo para mostrar los Provedores
         public DataSet showEmployees()
         {
-            return objPrv.showEmployees();
+            return objEmp.showEmployees();
         }
 
         //Metodo para guardar un nuevo Proveedor
         public bool saveEmployee(int _identificacion, string _nombre, string _apellidos, string _celular, string _direccion, string _correo)
         {
-            return objPrv.saveEmployee(_identificacion, _nombre, _apellidos, _celular, _direccion, _correo);
+            return objEmp.saveEmployee(_identificacion, _nombre, _apellidos, _celular, _direccion, _correo);
         }
 
         //Metodo para actualizar un Proveedor
         public bool updateEmployee(int _id, int _identificacion, string _nombre, string _apellidos, string _celular, string _direccion, string _correo)
         {
-            return objPrv.updateEmployee(_id, _identificacion, _nombre, _apellidos, _celular, _direccion, _correo);
+            return objEmp.updateEmployee(_id, _identificacion, _nombre, _apellidos, _celular, _direccion, _correo);
         }
     }
 }
