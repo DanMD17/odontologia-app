50f722e974f8e7ad9af42aaee02f5c40161df04c
Daniel Meza <daniel733xd@gmail.com>
2024-11-01 17:46:11 -0500

    Se crean funcionalidades de WFAuxiliaries y se corrigen cosas de la capa logic y data de Auxiliaries

--- WebAppOdontologia/Presentation/WFAuxiliaries.aspx
+++ WebAppOdontologia/Presentation/WFAuxiliaries.aspx
@@ -1,5 +1,30 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Main.Master" AutoEventWireup="true" CodeBehind="WFAuxiliaries.aspx.cs" Inherits="Presentation.WFAuxiliaries" %>
+
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <%--Aki va todo lo de el formulario de Auxiliares--%>
+    <asp:TextBox ID="TBId" runat="server"></asp:TextBox>
+    <%--Funcion del auxiliar--%>
+    <asp:Label ID="Label1" runat="server" Text="Ingrese la funcion"></asp:Label>
+    <asp:TextBox ID="TBFunction" runat="server"></asp:TextBox>
+    <br />
+    <%--Nivel educativo del auxiliar--%>
+    <asp:Label ID="Label2" runat="server" Text="Ingrese el nivel educativo del auxiliar"></asp:Label>
+    <asp:TextBox ID="TBEducationalLevel" runat="server"></asp:TextBox>
+    <br />
+    <%--Seleccionar empleado--%>
+    <asp:Label ID="Label3" runat="server" Text="Seleccione un empleado"></asp:Label>
+    <asp:DropDownList ID="DDLEmployee" runat="server" CssClass="fromn-select"></asp:DropDownList>
+    <%--Botones guardar y actualizar--%>
+    <div>
+        <asp:Button ID="BtnSave" runat="server" Text="Guardar" OnClick="BtnSave_Click" />
+        <asp:Button ID="BtbUpdate" runat="server" Text="Actualizar" OnClick="BtbUpdate_Click" />
+        <asp:Label ID="LblMsg" runat="server" Text=""></asp:Label>
+    </div>
+    <br />
+    <%--Lista de Empleados--%>
+    <div>
+        <asp:GridView ID="GVAuxiliaries" runat="server"></asp:GridView>
+    </div>
 </asp:Content>
--- WebAppOdontologia/Presentation/WFAuxiliaries.aspx.cs
+++ WebAppOdontologia/Presentation/WFAuxiliaries.aspx.cs
@@ -1,17 +1,68 @@
 ﻿using System;
+using Logic;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Runtime.Remoting;
 
 namespace Presentation
 {
     public partial class WFAuxiliaries : System.Web.UI.Page
     {
+        AuxiliariesLog objAux = new AuxiliaresLog();
+        EmployeesLog objEmp = new EmployeesLog();
+
+        private int _idAux, _fkEmployee;
+        private string _function, _educationalLevel;
+        private bool executed;
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                //Aqui se invocan todos los metodos
+                showAssistants();
+                showEmployeesDDL();
+            }
+        }
 
+        private void showAssistants()
+        {
+            DataSet ds = new DataSet();
+            ds = objAux.showAssistants();
+            GVAuxiliaries.DataSource = ds;
+            GVAuxiliaries.DataBind();
+        }
+
+        //Metodo para mostrar los empleados en el DDL
+        private void showEmployeesDDL()
+        {
+            DDLEmployee.DataSource = objEmp.showEmployeesDDL();
+            DDLEmployee.DataValueField = "emp_id"; //Nombre de la llave primaria
+            DDLEmployee.DataTextField = "emp_nombre";
+            DDLEmployee.DataBind();
+            DDLEmployee.Items.Insert(0, "Seleccione");
+        }
+
+        protected void BtnSave_Click(object sender, EventArgs e)
+        {
+            _educationalLevel = TBEducationalLevel.Text;
+            _function = TBFunction.Text;
+            _fkEmployee = Convert.ToInt32(DDLEmployee.SelectedValue);
+
+            executed = objAux.saveAssistant(_fkEmployee, _function, _educationalLevel);
+
+            if (executed)
+            {
+                LblMsg.Text = "La secretaria se guardó exitosamente!";
+                showAssistants();
+            }
+            else
+            {
+                LblMsg.Text = "Error al guardar :(";
+            }
         }
     }
 }
