bf700263eeece9ac15489d71bdeb3ba343a6447a
Daniel Meza <daniel733xd@gmail.com>
2024-11-01 18:01:51 -0500

    Se crean laas funcionalidades de guardar y mostrar para WFSecretaries y se agrega una libreria en la capa logic de Secretaries

--- WebAppOdontologia/Presentation/WFSecretaries.aspx
+++ WebAppOdontologia/Presentation/WFSecretaries.aspx
@@ -2,4 +2,23 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div>
+        <asp:TextBox ID="TBId" runat="server"></asp:TextBox>
+        <asp:Label ID="Label1" runat="server" Text="Ingrese la funcion de la secretaria"></asp:Label>
+        <asp:TextBox ID="TBFunction" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="Label2" runat="server" Text="Ingrese los anios de experiencia"></asp:Label>
+        <asp:TextBox ID="TBYearsExp" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="Label3" runat="server" Text="Seleccione un empleado"></asp:Label>
+        <asp:DropDownList ID="DDLEmployee" runat="server" CssClass="fromn-select"></asp:DropDownList>
+    </div>
+    <div>  
+        <asp:Button ID="BtnSave" runat="server" Text="Guardar" OnClick="BtnSave_Click" />
+        <asp:Button ID="BtnUpdate" runat="server" Text="Actualizar" />
+        <asp:Label ID="LblMsg" runat="server" Text=""></asp:Label>
+    </div>
+    <div>
+        <asp:GridView ID="GVSecretaries" runat="server"></asp:GridView>
+    </div>
 </asp:Content>
--- WebAppOdontologia/Presentation/WFSecretaries.aspx.cs
+++ WebAppOdontologia/Presentation/WFSecretaries.aspx.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Logic;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,9 +11,58 @@ namespace Presentation
 {
     public partial class WFSecretaries : System.Web.UI.Page
     {
+        //Crear los objetos
+        SecretariesLog objSec = new SecretariesLog();
+        EmployeesLog objEmp = new EmployeesLog();
+
+        private int _idSec, _fkEmployee;
+        private string _function, _yearsExp;
+        private bool executed;
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                //Aqui se invocan todos los metodos
+                showSecretaries();
+                showEmployeesDDL();
+            }
+        }
 
+        private void showSecretaries()
+        {
+            DataSet ds = new DataSet();
+            ds = objSec.showSecretaries();
+            GVSecretaries.DataSource = ds;
+            GVSecretaries.DataBind();
+        }
+
+        //Metodo para mostrar los empleados en el DDL
+        private void showEmployeesDDL()
+        {
+            DDLEmployee.DataSource = objEmp.showEmployeesDDL();
+            DDLEmployee.DataValueField = "emp_id"; //Nombre de la llave primaria
+            DDLEmployee.DataTextField = "emp_nombre";
+            DDLEmployee.DataBind();
+            DDLEmployee.Items.Insert(0, "Seleccione");
+        }
+
+        protected void BtnSave_Click(object sender, EventArgs e)
+        {
+            _yearsExp = TBYearsExp.Text;
+            _function = TBFunction.Text;
+            _fkEmployee = Convert.ToInt32(DDLEmployee.SelectedValue);
+
+            executed = objSec.saveSecretary(_fkEmployee, _function, _yearsExp);
+
+            if (executed)
+            {
+                LblMsg.Text = "La secretaria se guardó exitosamente!";
+                showSecretaries();
+            }
+            else
+            {
+                LblMsg.Text = "Error al guardar :(";
+            }
         }
     }
 }
