a53b89921c8db4ba83f5fa75f6351014f0b8a77e
RichardM18 <richard.montenegro@estudiante.fup.edu.co>
2024-11-02 01:34:03 -0500

    Se crean las funcionalidades de guardar y mostrar de la clase WFPatients

--- WebAppOdontologia/Presentation/WFPatients.aspx
+++ WebAppOdontologia/Presentation/WFPatients.aspx
@@ -1,5 +1,43 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Main.Master" AutoEventWireup="true" CodeBehind="WFPatients.aspx.cs" Inherits="Presentation.WFPatients" %>
+
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <%--Formulario para Pacientes--%>
+    <asp:TextBox ID="TBId" runat="server" Visible="false"></asp:TextBox>
+    <%--Nombre--%>
+    <asp:Label ID="Label1" runat="server" Text="Ingrese el nombre"></asp:Label>
+    <asp:TextBox ID="TBName" runat="server"></asp:TextBox>
+    <br />
+    <%--Apellido--%>
+    <asp:Label ID="Label2" runat="server" Text="Ingrese el apellido"></asp:Label>
+    <asp:TextBox ID="TBLastName" runat="server"></asp:TextBox>
+    <br />
+    <%--Fecha de Nacimiento--%>
+    <asp:Label ID="Label3" runat="server" Text="Ingrese la fecha de nacimiento"></asp:Label>
+    <asp:TextBox ID="TBDateOfBirth" runat="server" TextMode="Date"></asp:TextBox>
+    <br />
+    <%--Dirección--%>
+    <asp:Label ID="Label4" runat="server" Text="Ingrese la dirección"></asp:Label>
+    <asp:TextBox ID="TBAddress" runat="server"></asp:TextBox>
+    <br />
+    <%--Celular--%>
+    <asp:Label ID="Label5" runat="server" Text="Ingrese el número de celular"></asp:Label>
+    <asp:TextBox ID="TBCellPhone" runat="server"></asp:TextBox>
+    <br />
+    <%--Correo Electrónico--%>
+    <asp:Label ID="Label6" runat="server" Text="Ingrese el correo"></asp:Label>
+    <asp:TextBox ID="TBEmail" runat="server"></asp:TextBox>
+    <br />
+    <%--Botones de Guardar y Actualizar--%>
+    <div>
+        <asp:Button ID="BtnSave" runat="server" Text="Guardar" OnClick="BtnSave_Click" />
+        <asp:Button ID="BtnUpdate" runat="server" Text="Actualizar" OnClick="BtnUpdate_Click" />
+        <asp:Label ID="LblMsg" runat="server" Text=""></asp:Label>
+    </div>
+    <br />
+    <%--Lista de Pacientes--%>
+    <div>
+        <asp:GridView ID="GVPatients" runat="server"></asp:GridView>
+    </div>
 </asp:Content>
--- WebAppOdontologia/Presentation/WFPatients.aspx.cs
+++ WebAppOdontologia/Presentation/WFPatients.aspx.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using Logic;
+using System.Data;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -9,9 +8,75 @@ namespace Presentation
 {
     public partial class WFPatients : System.Web.UI.Page
     {
+        PatientsLog objPat = new PatientsLog();
+
+        private int _patientId;
+        private string _name, _lastName, _address, _cellPhone, _email;
+        private DateTime _dateOfBirth;
+        private bool executed;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                showPatients();
+            }
+        }
 
+        // Método para mostrar pacientes
+        private void showPatients()
+        {
+            DataSet ds = new DataSet();
+            ds = objPat.showPatients();
+            GVPatients.DataSource = ds;
+            GVPatients.DataBind();
+        }
+
+        // Botón de guardar
+        protected void BtnSave_Click(object sender, EventArgs e)
+        {
+            _name = TBName.Text;
+            _lastName = TBLastName.Text;
+            _dateOfBirth = DateTime.Parse(TBDateOfBirth.Text);
+            _address = TBAddress.Text;
+            _cellPhone = TBCellPhone.Text;
+            _email = TBEmail.Text;
+
+            executed = objPat.savePatient(_name, _lastName, _dateOfBirth, _address, _cellPhone, _email);
+
+            if (executed)
+            {
+                LblMsg.Text = "El paciente se guardó exitosamente!";
+                showPatients();
+            }
+            else
+            {
+                LblMsg.Text = "Error al guardar el paciente.";
+            }
+        }
+
+        // Botón de actualizar
+        protected void BtnUpdate_Click(object sender, EventArgs e)
+        {
+            _patientId = int.Parse(TBId.Text);
+            _name = TBName.Text;
+            _lastName = TBLastName.Text;
+            _dateOfBirth = DateTime.Parse(TBDateOfBirth.Text);
+            _address = TBAddress.Text;
+            _cellPhone = TBCellPhone.Text;
+            _email = TBEmail.Text;
+
+            executed = objPat.updatePatient(_patientId, _name, _lastName, _dateOfBirth, _address, _cellPhone, _email);
+
+            if (executed)
+            {
+                LblMsg.Text = "El paciente se actualizó exitosamente!";
+                showPatients();
+            }
+            else
+            {
+                LblMsg.Text = "Error al actualizar el paciente.";
+            }
         }
     }
 }
