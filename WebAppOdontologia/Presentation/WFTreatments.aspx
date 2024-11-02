1fd71c4b1e8c2cdc69ea60c2082d31bcadcc6b70
RichardM18 <richard.montenegro@estudiante.fup.edu.co>
2024-11-02 01:47:35 -0500

    Se crean las funciones de guardar y mostrar para la clase de tratamientos

--- WebAppOdontologia/Presentation/WFTreatments.aspx
+++ WebAppOdontologia/Presentation/WFTreatments.aspx
@@ -1,5 +1,56 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Main.Master" AutoEventWireup="true" CodeBehind="WFTreatments.aspx.cs" Inherits="Presentation.WFTreatments" %>
+
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <%--Formulario de Tratamientos--%>
+    <asp:TextBox ID="TBId" runat="server" Visible="false"></asp:TextBox>
+    
+    <%--Nombre--%>
+    <asp:Label ID="Label1" runat="server" Text="Ingrese el nombre del tratamiento"></asp:Label>
+    <asp:TextBox ID="TBName" runat="server"></asp:TextBox>
+    <br />
+    
+    <%--Descripción--%>
+    <asp:Label ID="Label2" runat="server" Text="Ingrese la descripción"></asp:Label>
+    <asp:TextBox ID="TBDescription" runat="server"></asp:TextBox>
+    <br />
+    
+    <%--Fecha--%>
+    <asp:Label ID="Label3" runat="server" Text="Ingrese la fecha"></asp:Label>
+    <asp:TextBox ID="TBDate" runat="server" TextMode="Date"></asp:TextBox>
+    <br />
+    
+    <%--Observaciones--%>
+    <asp:Label ID="Label4" runat="server" Text="Ingrese las observaciones"></asp:Label>
+    <asp:TextBox ID="TBObservations" runat="server"></asp:TextBox>
+    <br />
+    
+    <%--FK Cita ID--%>
+    <asp:Label ID="Label5" runat="server" Text="ID de la cita"></asp:Label>
+    <asp:TextBox ID="TBFkCitaId" runat="server"></asp:TextBox>
+    <br />
+    
+    <%--FK Historial ID--%>
+    <asp:Label ID="Label6" runat="server" Text="ID del historial"></asp:Label>
+    <asp:TextBox ID="TBFkHistId" runat="server"></asp:TextBox>
+    <br />
+    
+    <%--FK Auxiliar ID--%>
+    <asp:Label ID="Label7" runat="server" Text="ID del auxiliar"></asp:Label>
+    <asp:TextBox ID="TBFkAuxId" runat="server"></asp:TextBox>
+    <br />
+    
+    <%--Botones guardar y actualizar--%>
+    <div>
+        <asp:Button ID="BtnSave" runat="server" Text="Guardar" OnClick="BtnSave_Click" />
+        <asp:Button ID="BtnUpdate" runat="server" Text="Actualizar" OnClick="BtnUpdate_Click" />
+        <asp:Label ID="LblMsg" runat="server" Text=""></asp:Label>
+    </div>
+    <br />
+    
+    <%--Lista de Tratamientos--%>
+    <div>
+        <asp:GridView ID="GVTreatments" runat="server"></asp:GridView>
+    </div>
 </asp:Content>
--- WebAppOdontologia/Presentation/WFTreatments.aspx.cs
+++ WebAppOdontologia/Presentation/WFTreatments.aspx.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using Logic;
+using System.Data;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -9,9 +8,74 @@ namespace Presentation
 {
     public partial class WFTreatments : System.Web.UI.Page
     {
+        TreatmentsLog objTreatments = new TreatmentsLog();
+
+        private int _treatmentId;
+        private string _name, _description, _observations;
+        private DateTime _date;
+        private int _fkCitaId, _fkHistId, _fkAuxId;
+        private bool executed;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                showTreatments();
+            }
+        }
 
+        private void showTreatments()
+        {
+            DataSet ds = objTreatments.showTreatments();
+            GVTreatments.DataSource = ds;
+            GVTreatments.DataBind();
+        }
+
+        protected void BtnSave_Click(object sender, EventArgs e)
+        {
+            _name = TBName.Text;
+            _description = TBDescription.Text;
+            _date = DateTime.Parse(TBDate.Text);
+            _observations = TBObservations.Text;
+            _fkCitaId = int.Parse(TBFkCitaId.Text);
+            _fkHistId = int.Parse(TBFkHistId.Text);
+            _fkAuxId = int.Parse(TBFkAuxId.Text);
+
+            executed = objTreatments.saveTreatment(_name, _description, _date, _observations, _fkCitaId, _fkHistId, _fkAuxId);
+
+            if (executed)
+            {
+                LblMsg.Text = "El tratamiento se guardó exitosamente!";
+                showTreatments();
+            }
+            else
+            {
+                LblMsg.Text = "Error al guardar el tratamiento.";
+            }
+        }
+
+        protected void BtnUpdate_Click(object sender, EventArgs e)
+        {
+            _treatmentId = int.Parse(TBId.Text);
+            _name = TBName.Text;
+            _description = TBDescription.Text;
+            _date = DateTime.Parse(TBDate.Text);
+            _observations = TBObservations.Text;
+            _fkCitaId = int.Parse(TBFkCitaId.Text);
+            _fkHistId = int.Parse(TBFkHistId.Text);
+            _fkAuxId = int.Parse(TBFkAuxId.Text);
+
+            executed = objTreatments.updateTreatment(_treatmentId, _name, _description, _date, _observations, _fkCitaId, _fkHistId, _fkAuxId);
+
+            if (executed)
+            {
+                LblMsg.Text = "El tratamiento se actualizó exitosamente!";
+                showTreatments();
+            }
+            else
+            {
+                LblMsg.Text = "Error al actualizar el tratamiento.";
+            }
         }
     }
 }
