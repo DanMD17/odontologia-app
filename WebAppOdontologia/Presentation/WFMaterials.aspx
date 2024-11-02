d76c7339bbb939aedfbbbdc6c2f2ae8ef1a184f1
Paola Rengifo <pololita1015@gmail.com>
2024-11-02 00:43:39 -0500

    Se crean funcionalidades de WFMaterials y se corrigen cosas de la capa logic

--- WebAppOdontologia/Presentation/WFMaterials.aspx
+++ WebAppOdontologia/Presentation/WFMaterials.aspx
@@ -2,4 +2,32 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <%--Aqui va todo lo de el formulario de Materiales--%>
+<asp:TextBox ID="TBId" runat="server"></asp:TextBox>
+<%--Nombre del material--%>
+<asp:Label ID="Label1" runat="server" Text="Ingrese el nombre del material"></asp:Label>
+<asp:TextBox ID="TBmaterialName" runat="server"></asp:TextBox>
+<br />
+<%--Descripcion del material--%>
+<asp:Label ID="Label2" runat="server" Text="Describa el material empleado"></asp:Label>
+<asp:TextBox ID="TBmaterialDescription" runat="server"></asp:TextBox>
+<br />
+<%--Cantidad del material--%>
+<asp:Label ID="Label3" runat="server" Text="Introduzca la cantidad de material utilizado"></asp:Label>
+<asp:TextBox ID="TBmaterialQuantity" runat="server"></asp:TextBox>
+<br />
+<%--Seleccionar tratamientp--%>
+<asp:Label ID="Label4" runat="server" Text="Seleccione un tratamiento"></asp:Label>
+<asp:DropDownList ID="DDLTreatments" runat="server" CssClass="fromn-select"></asp:DropDownList>
+<%--Botones guardar y actualizar--%>
+<div>
+    <asp:Button ID="BtnSave" runat="server" Text="Guardar" OnClick="BtnSave_Click" />
+    <asp:Button ID="BtbUpdate" runat="server" Text="Actualizar" OnClick="BtbUpdate_Click" />
+    <asp:Label ID="LblMsg" runat="server" Text=""></asp:Label>
+</div>
+<br />
+<%--Lista de Materiales--%>
+<div>
+    <asp:GridView ID="GVMateriales" runat="server"></asp:GridView>
+</div>
 </asp:Content>
--- WebAppOdontologia/Presentation/WFMaterials.aspx.cs
+++ WebAppOdontologia/Presentation/WFMaterials.aspx.cs
@@ -1,16 +1,72 @@
 ﻿using System;
+using Logic;
+using System.Data;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Runtime.Remoting;
 
 namespace Presentation
 {
     public partial class WFMaterials : System.Web.UI.Page
     {
+        //Se crean los objetos 
+        MaterialsLog objMat = new MaterialsLog();
+        TreatmentsLog objTreat = new TreatmentsLog();
+
+        private int _IdMat, _fkTrataId, _materialQuantity;
+        private string _materialName, _materialDescription;
+        private bool executed;
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                //Aqui se invocan todos los metodos
+
+                showMaterials();
+                showTreatmentsDDL();
+            }
+        }
+        private void showMaterials()
+        {
+            DataSet ds = new DataSet();
+            ds = objMat.showMaterials();
+            GVMateriales.DataSource = ds;
+            GVMateriales.DataBind();
+        }
+
+        //Metodo para mostrar los tratamientos en el DDL
+        private void showTreatmentsDDL()
+        {
+            DDLTreatments.DataSource = objTreat.showTreatmentsDDL();
+            DDLTreatments.DataValueField = "trata_id"; //Nombre de la llave primaria
+            DDLTreatments.DataTextField = "trata_nombre";
+            DDLTreatments.DataBind();
+            DDLTreatments.Items.Insert(0, "Seleccione");
+        }
+
+        protected void BtnSave_Click(object sender, EventArgs e)
+        {
+            _materialDescription = TBmaterialDescription.Text;
+            _materialName = TBmaterialName.Text;
+            _materialQuantity = Convert.ToInt32(DDLTreatments.SelectedValue);
+            _fkTrataId = Convert.ToInt32(DDLTreatments.SelectedValue);
+
+
+            executed = objMat.saveMaterial(_materialDescription, _materialName, _materialQuantity, _fkTrataId);
+
+            if (executed)
+            {
+
+                LblMsg.Text = "El material se guardó exitosamente!";
+                showMaterials();
+            }
+            else
+            {
+                LblMsg.Text = "Error al guardar ";
+            }
 
         }
     }
