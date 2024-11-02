1482133aad88ae86b3a73b1be5c51aab664cd342
Tatiana Ordoñez <tatiana.2004.morales@gmail.com>
2024-11-01 23:31:41 -0500

    Se agregaron las funcionalidades de mostrar y guardar en WFDntists

--- WebAppOdontologia/Presentation/WFDentists.aspx
+++ WebAppOdontologia/Presentation/WFDentists.aspx
@@ -1,5 +1,27 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Main.Master" AutoEventWireup="true" CodeBehind="WFDentists.aspx.cs" Inherits="Presentation.WFDentists" %>
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Main.Master" AutoEventWireup="true" CodeBehind="WFDentist.aspx.cs" Inherits="Presentation.WFDentists" %>
+
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+    <%--Formulario de Odontólogos--%>
+    <asp:Label ID="Label1" runat="server" Text="Ingrese la especialidad"></asp:Label>
+    <asp:TextBox ID="TBSpecialty" runat="server"></asp:TextBox>
+    <br />
+    
+    <%--Seleccionar empleado--%>
+    <asp:Label ID="Label2" runat="server" Text="Seleccione un empleado"></asp:Label>
+    <asp:DropDownList ID="DDLEmployee" runat="server" CssClass="form-select"></asp:DropDownList>
+    
+    <%--Botones guardar--%>
+    <div>
+        <asp:Button ID="BtnSave" runat="server" Text="Guardar" OnClick="BtnSave_Click" />
+        <asp:Label ID="LblMsg" runat="server" Text=""></asp:Label>
+    </div>
+    <br />
+    
+    <%--Lista de Odontólogos--%>
+    <div>
+        <asp:GridView ID="GVDentists" runat="server"></asp:GridView>
+    </div>
 </asp:Content>
--- WebAppOdontologia/Presentation/WFDentists.aspx.cs
+++ WebAppOdontologia/Presentation/WFDentists.aspx.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Logic;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,9 +11,58 @@ namespace Presentation
 {
     public partial class WFDentists : System.Web.UI.Page
     {
+        DentistsLog objDen = new DentistsLog();
+        EmployeesLog objEmp = new EmployeesLog();
+
+        private int _idDen, _fkEmployee;
+        private string _specialty;
+        private bool executed;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                //Aqui se invocan todos los metodos
+                showDentists();
+                showEmployeesDDL();
+            }
+        }
 
+        private void showDentists()
+        {
+            DataSet ds = new DataSet();
+            ds = objDen.showDentists();
+            GVDentists.DataSource = ds;
+            GVDentists.DataBind();
+        }
+
+        //Metodo para mostrar los empleados en el DDL
+        private void showEmployeesDDL()
+        {
+            DDLEmployee.DataSource = objEmp.showEmployeesDDL();
+            DDLEmployee.DataValueField = "emp_id"; //Nombre de la llave primaria
+            DDLEmployee.DataTextField = "emp_nombre";
+            DDLEmployee.DataBind();
+            DDLEmployee.Items.Insert(0, "Seleccione");
+        }
+
+        protected void BtnSave_Click(object sender, EventArgs e)
+        {
+            _specialty = TBSpecialty.Text;
+            _fkEmployee = Convert.ToInt32(DDLEmployee.SelectedValue);
+
+            executed = objDen.saveDentist(_specialty, _fkEmployee);
+
+            if (executed)
+            {
+                LblMsg.Text = "El Odontólogo se guardó exitosamente!";
+                showDentists();
+            }
+            else
+            {
+                LblMsg.Text = "Error al guardar";
+            }
         }
     }
 }
+    
