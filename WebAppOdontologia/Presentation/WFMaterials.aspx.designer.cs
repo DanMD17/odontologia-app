ae26321d3f567195abd6c1f06e8192e1146c3386
Paola Rengifo <pololita1015@gmail.com>
2024-11-09 01:11:56 -0500

    Se actualizan las funcionalidades de Materiales en la capa de presentación, se añaden las funcionalidades de actualizar y eliminar y tambien librerias

--- WebAppOdontologia/Presentation/WFMaterials.aspx.designer.cs
+++ WebAppOdontologia/Presentation/WFMaterials.aspx.designer.cs
@@ -121,14 +121,5 @@ namespace Presentation
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label LblMsg;
-
-        /// <summary>
-        /// Control GVMateriales.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.GridView GVMateriales;
     }
 }
