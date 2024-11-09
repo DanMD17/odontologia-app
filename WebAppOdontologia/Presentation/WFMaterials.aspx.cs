7b56b313a7f0c2e01c326cb1237b282f053205ff
PaoR23 <159581380+PaoR23@users.noreply.github.com>
2024-11-09 11:14:36 -0500

    Se referencian librerias

--- WebAppOdontologia/Presentation/WFMaterials.aspx.cs
+++ WebAppOdontologia/Presentation/WFMaterials.aspx.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Web;
+using System.Linq;
 using System.Web.Services;
 using System.Web.UI;
 using System.Web.UI.WebControls;
